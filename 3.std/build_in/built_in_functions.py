e2d98f1780d74c5b914a1cfa0f2a3955e26bfcd4
ArgentoAskia <Askiaxiaoming@outlook.com>
2026-05-04 21:51:38 +0800

    2026.5.4 类型转换内置函数Demo

--- 3.std/build_in/built_in_functions.py
+++ 3.std/build_in/built_in_functions.py
@@ -39,6 +39,73 @@ print(f"1, 5, 3中的最小值是 = {ret}")
 print("================= 数学函数结束 =================")
 print()
 
+print("================= 内置函数开始 =================")
+
+## 1. 类型构造器函数
+## 1.1 int(x[, base=10]) ==> 将数字或字符串转为整数。base表示参数x是什么进制的。默认10
+ret = int('101', 2)
+print(f"101的2进制字符串 = {ret}")
+## 1.2 float(x) ==> 将数字或字符串转为浮点数。(实际上并不是函数而是构造方法)
+ret = float('3.14')
+print(f"'3.14'字符串转float = {ret}")
+
+ret = complex(2, 3)
+print(f"复数 = {ret}")
+
+ret = str(123)
+print(f"整数123转字符串 = {ret}")
+
+ret = bytes('hello', 'utf-8')
+print(f"('hello', 'utf-8')转不可变bytes数组 = {ret}")
+
+ret = bytearray('hello', 'utf-8')
+print(f"('hello', 'utf-8')转可变bytearray = {ret}")
+
+ret = list('abc')
+print(f"转list('abc') = {ret}")
+
+ret = tuple([1,2])
+print(f"转tuple([1,2]) = {ret}")
+
+ret = dict(a=1, b=2)
+print(f"转dict(a=1, b=2) = {ret}")
+
+ret = set('123')
+print(f"转set('123') = {ret}")
+
+ret = frozenset([1,2,3])
+print(f"转frozenset([1,2,3]) = {ret}")
+
+ret = bool([])
+print(f"转bool([]) = {ret}")
+
+## 2. chr(i) ==> 将整数（0-1114111）转为 Unicode 字符。
+ret = chr(65)
+print(f"65的ASCII码值：chr(65) = {ret}")
+
+## 3. ord(c) ==> 将字符转为 Unicode 码点。
+ret = ord('A')
+print(f"'A'的ASCII整数：ord('A') = {ret}")
+
+## 4. hex(x) ==> 转16进制字符串
+ret = hex(255)
+print(f"'255'的16进制字符串：hex(255) = {ret}")
+
+## 5. oct(x) ==> 转8进制字符串
+ret = oct(255)
+print(f"'255'的8进制字符串：oct(255) = {ret}")
+
+## 6. bin(x) ==> 转二进制字符串
+ret = bin(255)
+print(f"'255'的二进制字符串：bin(255) = {ret}")
+print("================= 内置函数结束 =================")
+
+
+
+
+
+
+
 
 
 
